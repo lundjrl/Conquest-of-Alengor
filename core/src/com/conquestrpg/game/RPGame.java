8ea57b84e3edb6c02e9cde574bdb8a37e95128ad
zachary-thomas <zachy.thomas@gmail.com>
2018-10-18 22:17:45 -0400

    Upload files.

--- core/src/com/conquestrpg/game/RPGame.java
+++ core/src/com/conquestrpg/game/RPGame.java
@@ -17,8 +17,8 @@ public class RPGame extends ApplicationAdapter {
 	}
 
 	@Override
-	public void render () { 
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render () {
+		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
