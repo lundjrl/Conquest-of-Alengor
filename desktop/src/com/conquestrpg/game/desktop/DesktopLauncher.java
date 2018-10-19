0105c7a3554f76a38574d39a78fca2f6f465e8ef
zachary-thomas <zachy.thomas@gmail.com>
2018-10-18 21:54:36 -0400

    Upload files.

---
+++ desktop/src/com/conquestrpg/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.conquestrpg.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.conquestrpg.game.RPGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RPGame(), config);
+	}
+}
