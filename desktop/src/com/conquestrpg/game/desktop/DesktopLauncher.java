3174c4755b0b4d1048880d4bf4ef6c32e6488e1c
zachary-thomas <zachy.thomas@gmail.com>
2018-10-20 14:18:53 -0400

    Updated DesktopLauncher

--- desktop/src/com/conquestrpg/game/desktop/DesktopLauncher.java
+++ desktop/src/com/conquestrpg/game/desktop/DesktopLauncher.java
@@ -1,7 +1,7 @@
 package com.conquestrpg.game.desktop;
 
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Application; // Set up window and logging
+import com.badlogic.gdx.Gdx; // Graphics, Audio, Input, Files and Net modules
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.conquestrpg.game.RPGame;
@@ -9,6 +9,22 @@ import com.conquestrpg.game.RPGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new RPGame(), config);
+
+		// Setup Configuration
+		config.title = "Conquest of Alengor"; // Title on window
+		config.useGL30 = false; // Use OpenGL 2.0
+		config.width = 800;
+		config.height = 600;
+
+		// Setup application
+		Application app = new LwjglApplication(new RPGame(), config);
+
+		// Logging
+		Gdx.app = app;
+		//Gdx.app.setLogLevel(Application.LOG_INFO);
+		//Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		//Gdx.app.setLogLevel(Application.LOG_ERROR);
+		//Gdx.app.setLogLevel(Application.LOG_NONE)
+
 	}
 }
