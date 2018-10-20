f14bbf2e6477a7d7ca58c9f9a67d028d195450ca
Zachary Thomas <zachy.thomas@gmail.com>
2018-10-20 13:23:32 -0400

    Added settings to tilemaps.

--- desktop/src/com/conquestrpg/game/desktop/DesktopLauncher.java
+++ desktop/src/com/conquestrpg/game/desktop/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package com.conquestrpg.game.desktop;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.conquestrpg.game.RPGame;
