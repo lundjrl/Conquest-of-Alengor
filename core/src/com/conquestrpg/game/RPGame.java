7606a609ba9fdaf06b998ef0f7ba950ebc1ec2ac
Matt Doan <doanma@mail.gvsu.edu>
2018-11-05 13:30:54 -0500

    Added many comments

--- core/src/com/conquestrpg/game/RPGame.java
+++ core/src/com/conquestrpg/game/RPGame.java
@@ -41,7 +41,7 @@ public class RPGame extends ApplicationAdapter implements InputProcessor {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.update();
+		camera.update(); //This changes camera position
 		tiledMapRenderer.setView(camera);
 		tiledMapRenderer.render();
 
