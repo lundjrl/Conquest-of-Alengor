02f770475fabb86aaa6d13f6f20c13a86cacf9e9
zachary-thomas <zachy.thomas@gmail.com>
2018-12-05 00:59:19 -0500

    Added comments for NPC

--- core/src/com/conquestrpg/game/NPC.java
+++ core/src/com/conquestrpg/game/NPC.java
@@ -15,35 +15,75 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
+/**
+ * This class is used as the NPC main class that mainly loads the
+ * object from the tilemap to the corresponding location along with
+ * the image for the NPC.
+ *
+ * @author James Lund, Zachary Thomas, Matthew Doan, Aaron Brinkerhoff
+ * @version Dec-4-2018
+ */
+
 public class NPC {
 
-    // Player
-    protected SpriteBatch character;
-    protected Texture texture;
-    protected Sprite sprite;
-    protected Rectangle playerBox;
-    protected MapObject npcObject;
-    protected float npcX;
-    protected float npcY;
-    protected float offset = 8.0f; // pixel offset for player collision
+    /** SpriteBatch that holds the character*/
+    private SpriteBatch character;
 
+    /** Texture to hold image file*/
+    private Texture texture;
+
+    /** Sprite to render to screen*/
+    private Sprite sprite;
+
+    /** Player box used for collisions, door handling and monsters*/
+    private Rectangle playerBox;
+
+    /** Holds the location of where to load NPC*/
+    protected MapObject npcObject;
+
+    /** Holds the location of the npc X*/
+    protected float npcX;
+
+    /** Holds the location of the npc Y*/
+    protected float npcY;
+
+    /** Holds the amount of movement for the player*/
     protected float move;
+
+    /** Holds iterations of i*/
     protected float i;
 
+    /**
+     * The constructor for the NPC object tha makes the NPC
+     * from the coordinates of the MapObject and loads
+     * the image from a file in the assets folder.
+     *
+     * @param npcObject
+     * @param image
+     */
     NPC(MapObject npcObject, String image){
         // Character
         this.npcObject = npcObject;
         character = new SpriteBatch();
+        // Load file
         texture = new Texture(Gdx.files.internal(image));
         sprite = new Sprite(texture);
+
+        // Get coordinates from tile map
         npcX = ((RectangleMapObject) npcObject).getRectangle().getX();
         npcY = ((RectangleMapObject) npcObject).getRectangle().getY();
+
+        // Set position of NPC
         sprite.setPosition(npcX, npcY );
 
          // For if enemy
          //move = -0.3f;
     }
 
+    /**
+     * Renders the NPC to the game with movement.
+     * @return none
+     */
     public void render () {
 
         sprite.translateX(move);
@@ -71,22 +111,42 @@ public class NPC {
 
     }
 
+    /**
+     * Accessor method to grab the NPC X-coordinate
+     * @return npcX
+     */
     public float getNPCx(){
         return this.npcX;
     }
 
+    /**
+     * Accessor method to grab the NPC Y-coordinate
+     * @return npcY
+     */
     public float getNPCy(){
         return this.npcY;
     }
 
+    /**
+     * Accessor method that grabs the monster sprite
+     * @return sprite
+     */
     public Sprite getSprite(){
         return  this.sprite;
     }
 
+    /**
+     * Accessor method that grabs the character super
+     * @return super.character
+     */
     public SpriteBatch getCharacter(){
         return this.character;
     }
 
+    /**
+     * Accessor method which grabs the collision box around the player
+     * @return playerBox
+     */
     public Rectangle getPlayerBox(){
         return this.playerBox;
     }
